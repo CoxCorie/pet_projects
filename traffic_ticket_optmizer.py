e28504dbffae428d0e10ae03e90dd7f0de32f7b4
CoxCorie <cox.corie@gmail.com>
2021-06-13 00:58:31 -0400

    improve simulation
    simulation now prints a constant stream of blank text representing the flow of time. It also now calcuates driver schedule ahead of time, reducing latency.

--- traffic_ticket_optmizer.py
+++ traffic_ticket_optmizer.py
@@ -31,18 +31,13 @@ class DriverProfile:
         self.dollars_ticketed = mph_over_limit * 10
 
     # O(n*m) time where n is duration_mins, and m is self.drivers_per_min
-    def get_traffic_gap_mins(self, duration_mins=0):
-        """returns a list of random numbers representing minutes until the next driver"""
-        if duration_mins == 0:
-            return expon.ppf(random()) * self.mins_between_drivers
-        elapsed_mins = 0
-        
-        mins_list = []
+    def get_driver_schedule(self, duration_mins=60):
+        """returns a list of Driver times in minutes that are randomly spaced apart"""
+        elapsed_mins, schedule = 0, []
         while elapsed_mins < duration_mins:
-            mins = expon.ppf(random()) * self.mins_between_drivers
-            mins_list.append(mins)
-            elapsed_mins += mins
-        return mins_list
+            elapsed_mins += expon.ppf(random()) * self.mins_between_drivers
+            schedule.append(elapsed_mins)
+        return schedule
 
     def __str__(self):
         return f"{self.mph_over_limit}mph over"
@@ -62,14 +57,16 @@ class DriverProfile:
 
 class Driver:
     """represents an individual driver"""
-    def __init__(self, driverProfile, traffic_gap_mins):
+    def __init__(self, driverProfile, scheduled_min):
         self.profile = driverProfile
-        self.traffic_gap_mins = traffic_gap_mins
+        self.scheduled_min = scheduled_min
+        self.got_ticket = False
+        self.ticketing_cop = None
     
     def __str__(self):
         mph_over = self.profile.mph_over_limit
-        traffic_gap_mins = round(self.traffic_gap_mins, 2)
-        return f"{mph_over}mph over, {traffic_gap_mins} mins behind previous car" 
+        scheduled_min = round(self.scheduled_min, 2)
+        return f"{mph_over}mph over at min: {scheduled_min}" 
 
 class TrafficPattern:
     """a collection of DriverProfiles representing an overall traffic pattern"""
@@ -116,28 +113,41 @@ class TrafficPattern:
                 max_revenue = cur_revenue
         return target_profiles
     
-    # O((k*n*log(m)) time where k is drivers_per_min, n is duration_mins, and m is len(driver_profiles)
+    # O((n*log(m)) time where n is len(schedule), and m is len(driver_profiles)
     def get_drivers(self, duration_mins):
-        """returns a list of random drivers to iterate through during simulations"""
-        minutes = self.total_profile.get_traffic_gap_mins(duration_mins)        
-        profiles = choices(population=self.driver_profiles, weights=self.profile_weights, k=len(minutes))
-        return [Driver(a, b) for (a,b) in zip(profiles,minutes)]
+        """returns a list of random drivers assigned to a random schedule"""
+        schedule = self.total_profile.get_driver_schedule(duration_mins)        
+        profiles = choices(population=self.driver_profiles, weights=self.profile_weights, k=len(schedule))
+        return [Driver(a, b) for (a,b) in zip(profiles,schedule)]
+    
+    # O(n) time where n is len(driver_profiles)
+    def ticket_drivers(self, drivers, target_profiles):
+        """tickets the target profile of drivers as resources allow"""
+        min_cop_is_available = 0
+        for driver in drivers:
+            if driver.profile in target_profiles and driver.scheduled_min > min_cop_is_available:
+                driver.got_ticket = True
+                min_cop_is_available = driver.scheduled_min + self.ticket_mins
     
     # O((k*n*log(m)) time where k is drivers_per_min, n is duration_mins, and m is len(driver_profiles)
-    def simulate_traffic(self, target_profiles, realtime_scale=30, duration_mins=60):
-        """runs a simlation of drivers with the target_profiles getting ticketed as resources allow"""
+    def simulate_traffic(self, target_profiles, iter_secs=.3, duration_mins=600):
+        """runs a simulation of the traffic and ticketing patterns"""
         drivers = self.get_drivers(duration_mins)
-        remaining_ticket_mins = 0
-        while drivers:
-            driver = drivers.pop()
-            time.sleep(driver.traffic_gap_mins * 1/realtime_scale * 60)
-            if driver.profile in target_profiles and remaining_ticket_mins == 0:
-                remaining_ticket_mins = self.ticket_mins
-                print(f"{driver} (ticketed ${driver.profile.dollars_ticketed})")
-            else:
-                remaining_ticket_mins = max(0, remaining_ticket_mins - driver.traffic_gap_mins)
-                print(driver)
-                continue
+        self.ticket_drivers(drivers, target_profiles)
+        if len(drivers) == 0:
+            raise ValueError('the list of drivers is empty')
+            
+        driver = drivers.pop(0)
+        for duration_min in range(duration_mins):
+            time.sleep(iter_secs)
+            print('\n')
+            while driver.scheduled_min < duration_min:
+                if driver.got_ticket:
+                    print(f"{driver.profile} (ticketed ${driver.profile.dollars_ticketed} at min: {int(driver.scheduled_min)})")
+                    time.sleep(iter_secs*5)
+                else:
+                    print(driver.profile)
+                driver = drivers.pop(0)
 
 if __name__ == "__main__":
     trafficPattern = TrafficPattern([ # mph_over_limit | mins_between_drivers
@@ -149,4 +159,5 @@ if __name__ == "__main__":
     target_profiles = trafficPattern.get_target_profiles()
     max_revenue = TrafficPattern(target_profiles).get_revenue_per_hour()
     print(f"the max expected hourly ticket revenue of ${max_revenue} occurs when ticketing the top {len(target_profiles)} speed group(s)")
+    time.sleep(3)
     trafficPattern.simulate_traffic(target_profiles)
